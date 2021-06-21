12bcd722b5010da3d64a7095c0753ae263076d90
github username <aslongman22@gmail.com>
2021-06-21 17:41:42 -0400

    Page Components

--- src/Containers/PageContainer.js
+++ src/Containers/PageContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from '../Components/Home';
+import About from '../Components/About';
+import Cases from '../Components/Cases';
+import Contact from '../Components/Contact';
 
 function PageContainer(props){
     return (
         <div id="PageContainer">
+            <Route path='/' render={routerProps => <Home {...routerProps}/>}/>
+            <Route path='/about' render={routerProps => <About {...routerProps}/>}/>
+            <Route path='/cases' render={routerProps => <Cases {...routerProps}/>}/>
+            <Route path='/contact' render={routerProps => <Contact {...routerProps}/>}/>
         </div>
     )
 }
